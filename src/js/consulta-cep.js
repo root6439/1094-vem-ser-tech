d0b4520ab51bae502ae6979e6b03fbd0005470d0
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2024-01-15 21:08:59 -0300

    implementação do form cep

--- src/js/consulta-cep.js
+++ src/js/consulta-cep.js
@@ -1,7 +1,5 @@
 let formCep = document.forms[0];
 
-console.dir(formCep);
-
 formCep.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log(event);
@@ -11,21 +9,63 @@ formCep.addEventListener("submit", (event) => {
 });
 
 formCep[0].addEventListener("input", async (event) => {
-  console.log(event.target.value.length);
   if (event.target.value.length >= 8) {
-    let address = await getCep(event.target.value);
-    preencherInput(address.logradouro, 1);
-    preencherInput(address.bairro, 2);
-    preencherInput(address.localidade, 3);
-    formCep[4].disabled = false;
-    formCep[5].disabled = false;
+    let resp = await getCep(event.target.value);
+
+    if (resp.erro) {
+      event.target.classList.add("input-error");
+      let errorText = document.createElement("small");
+      errorText.classList.add("input-error");
+      errorText.textContent = "CEP inexistente";
+
+      // acessando a label através do array labels
+      // event.target.labels[0].classList.add("input-error");
+
+      // acessando a label através do parent
+      event.target.parentNode.childNodes[1].classList.add("input-error");
+
+      event.target.insertAdjacentElement("afterend", errorText);
+      return;
+    }
+
+    preencherForm(resp);
   }
 });
 
 async function getCep(cep) {
-  return await fetch(`https://viacep.com.br/ws/${cep}/json`).then((resp) =>
-    resp.json()
-  );
+  // usando dois awaits
+  // let resp = await fetch(`https://viacep.com.br/ws/${cep}/json`);
+  // return await resp.json();
+
+  try {
+    let resp = await fetch(`https://viacep.com.br/ws/${cep}/json`);
+    let respInJson = await resp.json();
+
+    return respInJson;
+  } catch (e) {
+    console.log(e);
+  }
+
+  // usando dois thens
+  // return fetch(`https://viacep.com.br/ws/${cep}/json`)
+  //   .then((resp) => resp.json())
+  //   .then(respInJson => {
+  //     if (respInJson.erro) {
+  //       console.log("Cep inexistente");
+  //     }
+  //   })
+
+  // return await fetch(`https://viacep.com.br/ws/${cep}/json`).then((resp) =>
+  //   resp.json()
+  // );
+}
+
+function preencherForm(address) {
+  preencherInput(address.logradouro, 1);
+  preencherInput(address.bairro, 2);
+  preencherInput(address.localidade, 3);
+  formCep[4].disabled = false;
+  formCep[5].disabled = false;
 }
 
 function preencherInput(value, input) {
