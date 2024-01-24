e7fb153e4051b0558877445530a5da5314fcb7f5
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2024-01-24 19:20:48 -0300

    sorteador

--- src/js/sorteador.js
+++ src/js/sorteador.js
@@ -4,62 +4,86 @@ let dados = [
   ["Brenda Rayane", "disponivel", "36:23:33"],
   ["Bruna Silva", "disponivel", "54:56:05"],
   ["Douglas Alves", "disponivel", "59:56:20"],
-  ["Douglas Poeta", "disponivel", "47:20:19"],
   ["Elaine Santin (Nanne)", "disponivel", "55:51:31"],
   ["Emily Miro", "disponivel", "33:11:49"],
   ["Eric AG", "disponivel", "47:02:45"],
-  ["Erick Garcia", "disponivel", "37:39:55"],
   ["Ewerton Fragoso", "disponivel", "03:57:07"],
-  ["Fabio Lima", "disponivel", "35:48:49"],
-  ["Felipe Matos", "disponivel", "29:33:50"],
   ["Fernando Tavares", "disponivel", "33:47:28"],
   ["Henricky Lima", "disponivel", "30:06:21"],
   ["Hiago Maitan", "disponivel", "11:41:14"],
   ["Ingrid Santana", "disponivel", "57:05:13"],
-  ["Layna Moraes", "disponivel", "06:29:06"],
   ["Leandro Bispo Matos", "disponivel", "10:03:37"],
-  ["Leandro Matos", "disponivel", "35:49:01"],
   ["Luana Maria Da Silva Pereira de Freitas", "disponivel", "18:27:31"],
   ["Lucas Gonçalves", "disponivel", "24:37:37"],
   ["Max Castro", "disponivel", "32:52:35"],
   ["Naira Souza N.", "disponivel", "08:54:31"],
-  ["Nicolas Soares", "disponivel", "11:45:08"],
-  ["Rafael Galdino", "disponivel", "44:28:52"],
   ["Reydson Barros", "disponivel", "12:18:25"],
   ["Rodrigo Feijao", "disponivel", "56:48:24"],
   ["Rodrigo 'dio' Dionissa", "disponivel", "23:55:12"],
-  ["Thayane Jarosz", "disponivel", "00:11:45"],
+  ["rodrigo carvalho", "disponivel", "14:28:13"],
   ["Thiago Freitas", "disponivel", "07:39:05"],
   ["Wild Barreto", "disponivel", "00:13:11"],
-  ["maiky caires", "disponivel", "54:14:21"],
-  ["rodrigo carvalho", "disponivel", "14:28:13"],
 ];
 
-const alunos = dados.map((aluno) => aluno[0]);
-console.log(alunos);
+let nomes = dados.map((aluno) => aluno[0]);
+
+console.log(nomes.length);
 
 let grupos = [];
-let grupo = [];
-
-alunos.forEach((aluno, index) => {
-  let randomNumber = Math.floor(Math.random() * 33);
-
-  let grupo = [];
-
-  grupo.push(aluno);
-  grupo.splice(index, 1);
-
-  if (grupo.length == 6) {
-    grupos[grupos.length + 1];
+function dividirGrupos(nomes, qtdePorGrupo) {
+  while (nomes.length > 0) {
+    for (let i = 0; i < qtdePorGrupo && nomes.length > 0; i++) {
+      let randomNumber = Math.floor(Math.random() * nomes.length);
+      let grupo = nomes.splice(randomNumber, qtdePorGrupo);
+      grupos.push(grupo);
+    }
   }
 
-  grupos.push(grupo);
-
-  console.log(alunos[randomNumber]);
-});
-
-function setGrupo() {
-  let randomNumber = Math.floor(Math.random() * 33);
+  console.log(grupos);
 }
 
+dividirGrupos(nomes, 6);
+
+for (let i = 0; i < grupos.length; i++) {
+  let h2 = document.createElement("h2");
+  h2.textContent = `Grupo ${i + 1}:`;
+  document.body.appendChild(h2);
+  grupos[i].forEach((element) => {
+    let li = document.createElement("li");
+    li.textContent = element;
+
+    document.body.appendChild(li);
+  });
+}
+
+// console.log(nomes);
+
+// let grupos = [];
+// let grupo = [];
+// let grupo2 = [];
+
+// for (let i = 0; i < nomes.length; i++) {
+
+//   for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+//   }
+
+// }
+
+// nomes.forEach((nome, index) => {
+//   let randomNumber = Math.floor(Math.random() * 34);
+
+//   console.log(nomes[index]);
+
+//   if (grupo.length == 6) {
+
+//   } else {
+//     grupo.push(nome[randomNumber]);
+//   }
+
+//   nomes.splice(index, 1);
+// });
+
 // nomes.forEach((nome) => {});
+
